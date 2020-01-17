0a135713b02abcb277d8a63137a7feb5c771171f
dvsvora <59840866+dvsvora@users.noreply.github.com>
2020-01-17 15:21:29 +0530

    Update effect.js

--- effect.js
+++ effect.js
@@ -120,21 +120,21 @@ $('document').ready(function(){
 		loopSeven();
 		
 		$(this).fadeOut('slow').delay(5000).promise().done(function(){
-			$('#cake_fadein').click();
+			$('#cake_fadein').fadeIn('slow');
 		});
 	});	
 
 	$('#cake_fadein').click(function(){
 		$('.cake').fadeIn('slow');
 		$(this).fadeOut('slow').delay(3000).promise().done(function(){
-			$('#light_candle').click();
+			$('#light_candle').fadeIn('slow');
 		});
 	});
 
 	$('#light_candle').click(function(){
 		$('.fuego').fadeIn('slow');
 		$(this).fadeOut('slow').promise().done(function(){
-			$('#wish_message').click();
+			$('#wish_message').fadeIn('slow');
 		});
 	});
 
